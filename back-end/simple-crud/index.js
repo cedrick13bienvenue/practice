404eb8a1648b3309bb7220edab35002863bfe02c
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-04-11 19:57:58 +0200

    connecting db to node api

--- back-end/simple-crud/index.js
+++ back-end/simple-crud/index.js
@@ -1,10 +1,20 @@
 const express = require('express')
+const mongoose = require('mongoose');
 const app = express()
 
-app.listen(3000, ()=> {
-    console.log("Server is running on port 3000")
-})
 
 app.get('/',(req, res)=>{
     res.send("Hello from Node API!")
+})
+
+mongoose.connect('mongodb+srv://cedrick13bienvenue:srk4cedrick@backend-db.bihfz7h.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backend-db')
+.then(() => {
+    console.log('Connected to database!');
+    app.listen(3000, ()=> {
+        console.log("Server is running on port 3000")
+    })
+})
+    
+.catch(()=>{
+    console.log("Connection failed")
 })
