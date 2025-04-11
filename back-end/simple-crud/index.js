832bff710262ba08f264f0ee95f51bb1428d504e
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-04-11 20:44:48 +0200

    product schema

--- back-end/simple-crud/index.js
+++ back-end/simple-crud/index.js
@@ -2,9 +2,15 @@ const express = require('express')
 const mongoose = require('mongoose');
 const app = express()
 
+app.use(express.json())
 
 app.get('/',(req, res)=>{
     res.send("Hello from Node API!")
+})
+
+app.post('/api/products', (req, res)=>{
+    console.log(req.body)
+    res.send(req.body)
 })
 
 mongoose.connect('mongodb+srv://cedrick13bienvenue:srk4cedrick@backend-db.bihfz7h.mongodb.net/Node-API?retryWrites=true&w=majority&appName=backend-db')
