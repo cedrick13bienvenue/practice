ab33804601a024bd794a0f6f5bfe919886eedd30
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-04-23 12:41:01 +0200

    Adding a product into a DB

--- back-end/simple-crud/index.js
+++ back-end/simple-crud/index.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose');
+const Product = require('./models/product.model')
 const app = express()
 
 
@@ -12,9 +13,13 @@ app.get('/',(req, res)=>{
     res.send("Hello from Node API!")
 })
 
-app.post('/api/products', (req, res)=>{
-    console.log("Request Body:",req.body)
-    res.send(req.body)
+app.post('/api/products', async (req, res)=>{
+    try {
+       const product =  await Product.create(req.body);
+       res.status(200).json(product)
+    } catch (error){
+       res.status(500).json({message: error.message})
+    }
 })
 
 
