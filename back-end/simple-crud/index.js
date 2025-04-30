e35d72f2496bcb0e9f30b73af1fc9b43acb80155
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-04-30 18:08:50 +0200

    creating read Id API

--- back-end/simple-crud/index.js
+++ back-end/simple-crud/index.js
@@ -1,43 +1,53 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const mongoose = require('mongoose');
-const Product = require('./models/product.model')
-const app = express()
+const express = require("express");
+const mongoose = require("mongoose");
+const Product = require("./models/product.model");
+const app = express();
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/',(req, res)=>{
-    res.send("Hello from Node API!")
-})
+app.get("/", (req, res) => {
+  res.send("Hello from Node API!");
+});
 
-app.get('/api/products', async (req,res) => {
-     try {
-        const products = await Product.find({})
-        res.status(200).json(products)
-     }  catch (error) {
-        res.status(500).json({message : error.message})
+app.get("/api/products", async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-     }  
-})
+app.get("/api/products/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findById(id);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-app.post('/api/products', async (req, res)=>{
-    try {
-       const product =  await Product.create(req.body);
-       res.status(200).json(product)
-    } catch (error){
-       res.status(500).json({message: error.message})
-    }
-})
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
-mongoose.connect(process.env.MONGO_URI)
-.then(() => {
-    console.log('Connected to database!');
-    app.listen(3000, ()=> {
-        console.log("Server is running on port 3000")
-    })
-})
-    
-.catch(()=>{
-    console.log("Connection failed")
-})
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to database!");
+    app.listen(3000, () => {
+      console.log("Server is running on port 3000");
+    });
+  })
+
+  .catch(() => {
+    console.log("Connection failed");
+  });
