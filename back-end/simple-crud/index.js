66b0bb7b6cdd2a3d129f7ac4b0457edd17b789fe
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-04-30 23:47:23 +0200

    update API

--- back-end/simple-crud/index.js
+++ back-end/simple-crud/index.js
@@ -39,6 +39,22 @@ app.post("/api/products", async (req, res) => {
   }
 });
 
+// update a product
+
+app.put("/api/product/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body);
+    if (!product) {
+      return res.status(404).json({ message: "Product not found" });
+    }
+    const updatedProduct = await Product.findById(id);
+    res.status(200).json(updatedProduct);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
