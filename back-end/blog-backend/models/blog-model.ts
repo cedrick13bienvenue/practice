03b034380de786caf1b0f295e2193cfc8269bea1
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-15 22:01:48 +0200

    feat: create blog-model.ts mongoose schema and model

--- back-end/blog-backend/models/blog-model.ts
+++ back-end/blog-backend/models/blog-model.ts
@@ -1,12 +1,17 @@
 import mongoose from 'mongoose';
 
-const blogSchema = new mongoose.Schema({
-  title: { type: String, required: true, minlength: 3 },
-  name: { type: String, required: true, minlength: 3 },
-  content: { type: String, required: true, minlength: 10 },
-  updated: { type: Date, default: Date.now },
-});
+const blogSchema = new mongoose.Schema(
+  {
+    slug: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    author: { type: String, required: true },
+    content: { type: String, required: true },
+    isPublished: { type: Boolean, required: true },
+    deletedAt: { type: Date, default: null },
+  },
+  { timestamps: true }
+);
 
 const Blog = mongoose.model('Blog', blogSchema);
-
 export default Blog;
