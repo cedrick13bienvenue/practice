3b9f7166163357754c866aa6100f1fa2052c2f7d
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-16 08:01:24 +0200

    feat: define BlogInterface in types/blogTypes.ts

---
+++ back-end/blog-backend/types/blogTypes.ts
@@ -0,0 +1,12 @@
+export interface BlogInterface {
+  _id: string;
+  slug: string;
+  title: string;
+  author: string;
+  content: string;
+  isPublished: boolean;
+  description: string;
+  createdAt: string;
+  updatedAt: string;
+  deletedAt: null | string | undefined;
+}
