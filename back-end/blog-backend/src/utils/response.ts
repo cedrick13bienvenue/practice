eee739eb8d90b99f7749961cb83f7f3f3a1b4587
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-16 09:46:50 +0200

    feat: add custom response service

--- back-end/blog-backend/src/utils/response.ts
+++ back-end/blog-backend/src/utils/response.ts
@@ -1,41 +1,23 @@
 import { Response } from 'express';
-import { ApiResponse } from '../types/blog-types';
 
-export const sendResponse = <T>(
-  res: Response,
-  statusCode: number,
-  success: boolean,
-  message?: string,
-  data?: T,
-  error?: string,
-  pagination?: any
-): Response<ApiResponse<T>> => {
-  const response: ApiResponse<T> = {
+interface IResponse<T> {
+  res: Response;
+  data?: T;
+  message?: string;
+  status?: number;
+  success?: boolean;
+}
+
+export const ResponseService = <T>({
+  res,
+  data,
+  message = '',
+  status = 200,
+  success = true,
+}: IResponse<T>) => {
+  return res.status(status).json({
     success,
     message,
     data,
-    error,
-    pagination
-  };
-
-  return res.status(statusCode).json(response);
-};
-
-export const sendSuccess = <T>(
-  res: Response, 
-  data?: T, 
-  message?: string, 
-  statusCode: number = 200,
-  pagination?: any
-) => {
-  return sendResponse(res, statusCode, true, message, data, undefined, pagination);
-};
-
-export const sendError = (
-  res: Response, 
-  message: string, 
-  statusCode: number = 500, 
-  error?: string
-) => {
-  return sendResponse(res, statusCode, false, message, undefined, error);
+  });
 };
