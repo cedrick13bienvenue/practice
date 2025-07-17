adac08836e45f1543015d0e573cd608604a48b3e
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-17 08:51:53 +0200

    feat: add PUT and DELETE blog routes with validation

--- back-end/blog-backend/src/routes/blog-routes.ts
+++ back-end/blog-backend/src/routes/blog-routes.ts
@@ -3,9 +3,15 @@ import {
   createBlog,
   getAllBlogs,
   getABlog,
+  updateBlog,      
+  deleteBlog,     
 } from '../controllers/blog-controller';
 import { ValidationMiddleware } from '../middlewares/validate-blog';
-import { AddBlogSchema, IdValidationSchema } from '../schemas/blog-schema';
+import {
+  AddBlogSchema,
+  IdValidationSchema,
+  UpdateBlogSchema,  
+} from '../schemas/blog-schema';
 
 export const blogRouter = Router();
 
@@ -19,4 +25,17 @@ blogRouter.post(
   '/blogs',
   ValidationMiddleware({ type: 'body', schema: AddBlogSchema }),
   createBlog
+);
+
+blogRouter.put(
+  '/blogs/:id',
+  ValidationMiddleware({ type: 'params', schema: IdValidationSchema }),
+  ValidationMiddleware({ type: 'body', schema: UpdateBlogSchema }),
+  updateBlog
+);
+
+blogRouter.delete(
+  '/blogs/:id',
+  ValidationMiddleware({ type: 'params', schema: IdValidationSchema }),
+  deleteBlog
 );
