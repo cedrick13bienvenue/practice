2c03eabf1dbe05890fb06e9a535daaec67893400
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-17 13:10:27 +0200

    chore(server): register auth routes and ensure dotenv config is loaded

--- back-end/blog-backend/index.ts
+++ back-end/blog-backend/index.ts
@@ -3,6 +3,7 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './src/config/db';
 import { blogRouter } from './src/routes/blog-routes';
+import { authRouter } from './src/routes/auth-routes'; 
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 
 // Routes
 app.use('/api', blogRouter);
+app.use('/api/auth', authRouter);
 
 // Root route
 app.get('/', (_req, res) => {
