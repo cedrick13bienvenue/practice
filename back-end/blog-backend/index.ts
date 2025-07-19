aef85ef011d07dc929eb8be7828d3bfcd6c02feb
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-19 23:38:11 +0200

    Register comment and like routes in main application

--- back-end/blog-backend/index.ts
+++ back-end/blog-backend/index.ts
@@ -3,7 +3,9 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import { connectDB } from './src/config/db';
 import { blogRouter } from './src/routes/blog-routes';
-import { authRouter } from './src/routes/auth-routes'; 
+import { authRouter } from './src/routes/auth-routes';
+import { commentRouter } from './src/routes/comment-routes';
+import { likeRouter } from './src/routes/like-routes';
 
 dotenv.config();
 
@@ -17,6 +19,8 @@ app.use(express.json());
 // Routes
 app.use('/api', blogRouter);
 app.use('/api/auth', authRouter);
+app.use('/api', commentRouter);
+app.use('/api', likeRouter);
 
 // Root route
 app.get('/', (_req, res) => {
