13a8206761aac57f1439a09735cd2dbad5063864
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-22 23:07:10 +0200

    refactor: remove validation middleware and schema imports after migration to Sequelize

--- back-end/blog-backend/src/routes/like-routes.ts
+++ back-end/blog-backend/src/routes/like-routes.ts
@@ -3,7 +3,6 @@ import { toggleLike, getBlogLikes } from '../controllers/like-controller';
 import { protect } from '../middlewares/protect';
 import { roleChecker } from '../middlewares/role-checker';
 import { ValidationMiddleware } from '../middlewares/validate-blog';
-import { BlogIdSchema } from '../schemas/like-schema';
 
 export const likeRouter = Router();
 
@@ -11,12 +10,10 @@ likeRouter.post(
   '/blogs/:blogId/like',
   protect,
   roleChecker(['user']),
-  ValidationMiddleware({ type: 'params', schema: BlogIdSchema }),
   toggleLike
 );
 
 likeRouter.get(
   '/blogs/:blogId/likes',
-  ValidationMiddleware({ type: 'params', schema: BlogIdSchema }),
   getBlogLikes
 );
