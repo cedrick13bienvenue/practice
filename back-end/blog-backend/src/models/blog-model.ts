2dcbf8dc91eadd50eb822473127e891ef2da03fe
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-22 22:48:00 +0200

    refactor: convert blog model from Mongoose to Sequelize, fix author type to integer, add typings and associations

--- back-end/blog-backend/src/models/blog-model.ts
+++ back-end/blog-backend/src/models/blog-model.ts
@@ -1,16 +1,58 @@
-import mongoose, { Schema, model } from 'mongoose';
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../config/db';
 
-const blogSchema = new Schema({
-  title: String,
-  slug: String,
-  description: String,
-  author: String,
-  content: String,
-  image: { type: String, default: null }, 
-  isPublished: Boolean,
-  createdAt: Date,
-  updatedAt: { type: Date, default: Date.now },
-  deletedAt: { type: Date, default: null },
-});
+export interface BlogAttributes {
+  id?: number;
+  title: string;
+  slug: string;
+  description: string;
+  author: number;
+  content: string;
+  image?: string;
+  isPublished?: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+  deletedAt?: Date;
+}
 
-export const blogModel = model('blogs', blogSchema);
+export class BlogModel extends Model<BlogAttributes> implements BlogAttributes {
+  public id!: number;
+  public title!: string;
+  public slug!: string;
+  public description!: string;
+  public author!: number;
+  public content!: string;
+  public image!: string;
+  public isPublished!: boolean;
+  public createdAt!: Date;
+  public updatedAt!: Date;
+  public deletedAt!: Date;
+}
+
+BlogModel.init(
+  {
+    title: DataTypes.STRING,
+    slug: DataTypes.STRING,
+    description: DataTypes.STRING,
+    author: { type: DataTypes.INTEGER, allowNull: false },
+    content: DataTypes.STRING,
+    image: { type: DataTypes.STRING, defaultValue: null },
+    isPublished: DataTypes.BOOLEAN,
+    createdAt: DataTypes.DATE,
+    updatedAt: { type: DataTypes.DATE, defaultValue: DataTypes.NOW },
+    deletedAt: { type: DataTypes.DATE, defaultValue: null },
+  },
+  {
+    sequelize,
+    modelName: 'Blog',
+    tableName: 'blogs',
+    timestamps: false,
+  }
+);
+
+// Associations will be set up after all models are defined
+export const associateModels = (models: any) => {
+  BlogModel.belongsTo(models.UserModel, { foreignKey: 'author', targetKey: 'id' });
+  BlogModel.hasMany(models.CommentModel, { foreignKey: 'blog', sourceKey: 'id' });
+  BlogModel.hasMany(models.LikeModel, { foreignKey: 'blog', sourceKey: 'id' });
+};
