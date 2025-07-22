dd0049471f2c4ba9ad33f42f3bd61470c14242e1
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-22 23:07:42 +0200

    chore: remove obsolete Mongoose schema files after migrating to Sequelize

--- back-end/blog-backend/src/schemas/comment-schemas.ts
+++
@@ -1,5 +0,0 @@
-import Joi from 'joi';
-
-export const AddCommentSchema = Joi.object({
-  content: Joi.string().required(),
-});
--- back-end/blog-backend/src/schemas/like-schema.ts
+++
@@ -1,5 +0,0 @@
-import Joi from 'joi';
-
-export const BlogIdSchema = Joi.object({
-  blogId: Joi.string().length(24).required(),
-});
