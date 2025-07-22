fbde4e82c2249d42e1e87ba7355f5716184fdfe7
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-22 23:04:36 +0200

    refactor: remove validation middleware and schema imports after migration to Sequelize

--- back-end/blog-backend/src/routes/auth-routes.ts
+++ back-end/blog-backend/src/routes/auth-routes.ts
@@ -1,18 +1,17 @@
 import { Router } from 'express';
 import { registerUser, loginUser } from '../controllers/auth-controller';
 import { ValidationMiddleware } from '../middlewares/validate-blog';
-import { RegisterUserSchema, LoginUserSchema } from '../schemas/user-schema';
 
 export const authRouter = Router();
 
 authRouter.post(
   '/register',
-  ValidationMiddleware({ type: 'body', schema: RegisterUserSchema }),
+  // ValidationMiddleware({ type: 'body', schema: RegisterUserSchema }),
   registerUser
 );
 
 authRouter.post(
   '/login',
-  ValidationMiddleware({ type: 'body', schema: LoginUserSchema }),
+  // ValidationMiddleware({ type: 'body', schema: LoginUserSchema }),
   loginUser
 );
