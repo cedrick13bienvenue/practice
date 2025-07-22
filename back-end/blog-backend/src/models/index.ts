e4e070001c831d0cfbbc46851e1074bdea4aaf3c
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-22 23:03:27 +0200

    feat: add central models index to set up associations and sync Sequelize models

---
+++ back-end/blog-backend/src/models/index.ts
@@ -0,0 +1,20 @@
+import { sequelize } from '../config/db';
+import { UserModel, associateModels as associateUser } from './user-model';
+import { BlogModel, associateModels as associateBlog } from './blog-model';
+import { CommentModel, associateModels as associateComment } from './comment-model';
+import { LikeModel, associateModels as associateLike } from './like-model';
+
+const models = { UserModel, BlogModel, CommentModel, LikeModel };
+
+// Set up associations
+associateUser(models);
+associateBlog(models);
+associateComment(models);
+associateLike(models);
+
+// Sync models with the database
+export const syncModels = async () => {
+  await sequelize.sync({ alter: true }); // Use alter for dev, switch to false for prod
+};
+
+export { UserModel, BlogModel, CommentModel, LikeModel }; 
