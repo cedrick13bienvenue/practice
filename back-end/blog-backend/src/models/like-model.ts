6bc89413e90f2404f42d4f6b1e7e4edd4f939b08
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-22 23:03:19 +0200

    refactor: convert like model from Mongoose to Sequelize with TypeScript typings and associations

--- back-end/blog-backend/src/models/like-model.ts
+++ back-end/blog-backend/src/models/like-model.ts
@@ -1,11 +1,37 @@
-import { Schema, model, Types } from 'mongoose';
+import { DataTypes, Model } from 'sequelize';
+import { sequelize } from '../config/db';
 
-const likeSchema = new Schema(
+export interface LikeAttributes {
+  id?: number;
+  blog: number;
+  user: number;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+export class LikeModel extends Model<LikeAttributes> implements LikeAttributes {
+  public id!: number;
+  public blog!: number;
+  public user!: number;
+  public createdAt!: Date;
+  public updatedAt!: Date;
+}
+
+LikeModel.init(
   {
-    blog: { type: Types.ObjectId, ref: 'blogs', required: true },
-    user: { type: Types.ObjectId, ref: 'users', required: true },
+    blog: { type: DataTypes.INTEGER, allowNull: false }, // Will be associated later
+    user: { type: DataTypes.INTEGER, allowNull: false }, // Will be associated later
   },
-  { timestamps: true }
+  {
+    sequelize,
+    modelName: 'Like',
+    tableName: 'likes',
+    timestamps: true,
+  }
 );
 
-export const LikeModel = model('likes', likeSchema);
+// Associations will be set up after all models are defined
+export const associateModels = (models: any) => {
+  LikeModel.belongsTo(models.BlogModel, { foreignKey: 'blog', targetKey: 'id' });
+  LikeModel.belongsTo(models.UserModel, { foreignKey: 'user', targetKey: 'id' });
+};
