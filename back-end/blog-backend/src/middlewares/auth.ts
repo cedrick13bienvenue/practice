f8a81e7a789930a703cd2a592cf49fd79e8c31c1
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-24 14:48:52 +0200

    feat: update ensureAuthenticated to check for blacklisted sessions

--- back-end/blog-backend/src/middlewares/auth.ts
+++ back-end/blog-backend/src/middlewares/auth.ts
@@ -1,7 +1,12 @@
 import { Request, Response, NextFunction } from 'express';
+import { blacklistedSessions } from './session-blacklist';
 
 export function ensureAuthenticated(req: Request, res: Response, next: NextFunction) {
-  if (typeof req.isAuthenticated === 'function' && req.isAuthenticated()) {
+  if (
+    typeof req.isAuthenticated === 'function' &&
+    req.isAuthenticated() &&
+    !blacklistedSessions.has(req.sessionID)
+  ) {
     return next();
   }
   res.status(401).json({ message: 'Unauthorized' });
