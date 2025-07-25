a2fdb76db7a2483582f90631be18788afb5d8629
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-07-25 09:01:59 +0200

    docs: add Swagger comments for like routes

--- back-end/blog-backend/src/routes/like-routes.ts
+++ back-end/blog-backend/src/routes/like-routes.ts
@@ -6,6 +6,34 @@ import { ValidationMiddleware } from '../middlewares/validate-blog';
 
 export const likeRouter = Router();
 
+/**
+ * @swagger
+ * /blogs/{blogId}/like:
+ *   post:
+ *     summary: Like or unlike a blog (user only)
+ *     parameters:
+ *       - in: path
+ *         name: blogId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: Like toggled
+ *
+ * /blogs/{blogId}/likes:
+ *   get:
+ *     summary: Get likes for a blog
+ *     parameters:
+ *       - in: path
+ *         name: blogId
+ *         required: true
+ *         schema:
+ *           type: string
+ *     responses:
+ *       200:
+ *         description: List of likes
+ */
 likeRouter.post(
   '/blogs/:blogId/like',
   ensureAuthenticated,
