54ee62e01ad973a1cc4ac7bcce8090740c54fd90
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-08-03 16:38:41 +0200

    feat: integrate NewsletterSubscriber model into models index
    - Add NewsletterSubscriberModel import to models index
    - Include NewsletterSubscriberModel in models object
    - Add associateNewsletter function call for model associations
    - Export NewsletterSubscriberModel for use in other modules
    - Maintain existing model association patterns
    - Ensure proper model initialization order

--- back-end/blog-backend/src/models/index.ts
+++ back-end/blog-backend/src/models/index.ts
@@ -3,14 +3,16 @@ import { UserModel, associateModels as associateUser } from './user-model';
 import { BlogModel, associateModels as associateBlog } from './blog-model';
 import { CommentModel, associateModels as associateComment } from './comment-model';
 import { LikeModel, associateModels as associateLike } from './like-model';
+import { NewsletterSubscriberModel, associateModels as associateNewsletter } from './newsletter-subscriber-model';
 
-const models = { UserModel, BlogModel, CommentModel, LikeModel };
+const models = { UserModel, BlogModel, CommentModel, LikeModel, NewsletterSubscriberModel };
 
 // Set up associations
 associateUser(models);
 associateBlog(models);
 associateComment(models);
 associateLike(models);
+associateNewsletter(models);
 
 // Sync models with the database
 export const syncModels = async () => {
@@ -18,4 +20,4 @@ export const syncModels = async () => {
   await sequelize.sync({ alter: true }); // Use alter for dev, switch to false for prod
 };
 
-export { UserModel, BlogModel, CommentModel, LikeModel }; 
+export { UserModel, BlogModel, CommentModel, LikeModel, NewsletterSubscriberModel }; 
